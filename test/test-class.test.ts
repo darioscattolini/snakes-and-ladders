42a58755088259da3f0ffbb7b53c894caede58ce
Darío Scattolini <dario.scattolini@gmail.com>
2022-08-13 15:58:10 +0200

    add test: token starting position

--- test/test-class.test.ts
+++
@@ -1,8 +0,0 @@
-import { Test } from "../src/test-class"
-
-describe("test", () => {
-  it("should return 15 for add(10,5)", () => {
-    const test = new Test()
-    expect(test).not.toBeNull()
-  });
-});
