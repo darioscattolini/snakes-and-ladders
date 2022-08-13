c7c59417180aa35aa2efa0680af2d811360acd1b
Darío Scattolini <dario.scattolini@gmail.com>
2022-08-13 16:10:22 +0200

    pass test: moving token once

--- src/snake-and-ladders.ts
+++ src/snake-and-ladders.ts
@@ -2,6 +2,10 @@ export class SnakeAndLadders {
   private tokens = [1];
 
   public getTokenPosition(player: number) {
-    return this.tokens[player]
+    return this.tokens[player];
+  }
+
+  public moveToken(player: number, spaces: number) {
+    this.tokens[player] += spaces;
   }
 }
