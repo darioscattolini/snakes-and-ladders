e9b03c0021bf6eeb64b9115a79b458f5a1caea2c
Darío Scattolini <dario.scattolini@gmail.com>
2022-08-13 13:46:20 +0200

    set up TypeScript + Jest project

---
+++ test/test-class.test.ts
@@ -0,0 +1,8 @@
+import { Test } from "../src/test-class"
+
+describe("test", () => {
+  it("should return 15 for add(10,5)", () => {
+    const test = new Test()
+    expect(test).not.toBeNull()
+  });
+});
