fa963128e3675fbd21a418947ed6b9ed91d8ea4d
Darío Scattolini <dario.scattolini@gmail.com>
2022-08-13 22:14:44 +0200

    add main file to run game in terminal

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { SnakeAndLadders } from "./snake-and-ladders";
+import inquirer from 'inquirer';
+
+const game = new SnakeAndLadders();
+
+function askPlayer(message: String) {
+  inquirer.prompt([{
+    name: 'diceRolled',
+    type: 'confirm',
+    message: message 
+  }]).then(answers => {
+    if (answers['diceRolled'] == true) {
+      game.rollDice();
+      const dice = game.currentDiceRoll;
+      const token = game.getToken(1);
+
+      game.moveTokenOf(1);
+      
+      const message = `Dice was ${dice} and you moved to square ${token.position}.`;
+
+      if (game.getWinner() === -1) {
+        askPlayer(`${message} Roll dice again?`);
+      } else {
+        console.log('You reached square 100, you have won!')
+      }
+    }
+  })
+}
+
+askPlayer('Roll dice?');
